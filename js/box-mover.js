fdac22c338953dfa27f9b55675cc9d4419869560
adixime <adixime@gmail.com>
2012-01-20 12:53:45 -0500

    complete

--- js/box-mover.js
+++ js/box-mover.js
@@ -8,13 +8,14 @@
 var box; //Holds our <div> to move around
 
 var createBox = function (ev) {
+	removeActiveClass();
 	box = document.createElement('div'); // .createElement() makes a new empty HTML tag
 	
-	box.className = 'box'; //.className allows us to manipulate the classes of a tag
+	box.className = 'box active'; //.className allows us to manipulate the classes of a tag
 	
-	document.body.appendChild(box); //.appendChild() will add an element to the HTML file
+	document.documentElement.appendChild(box); //.appendChild() will add an element to the HTML file
 	
-	/*
+		/*
 		document.documentElement.clientWidth - the browser width
 		element.offsetWidth - the element's width
 	*/
@@ -24,13 +25,27 @@ var createBox = function (ev) {
 	
 	var randTop = Math.round(Math.random() * (document.documentElement.clientHeight - box.offsetHeight));
 	box.style.top = randTop + 'px';
-	
-	console.log(randLeft);
+
 };
 
 var moveDown = function (ev) {
-	var newTop = box.offsetTop + 10;
+	var newTop = box.offsetTop + 20;
 	box.style.top = newTop + 'px';
+};
+
+var moveUp = function (ev) {
+	var newTop = box.offsetTop - 20;
+	box.style.top = newTop + 'px';
+};
+
+var moveLeft = function (ev) {
+	var newLeft = box.offsetLeft - 20;
+	box.style.left = newLeft + 'px';
+};
+
+var moveRight = function (ev) {
+	var newLeft = box.offsetLeft + 20;
+	box.style.left = newLeft + 'px';
 };
 
 /*
@@ -40,3 +55,58 @@ var moveDown = function (ev) {
 document.getElementById('create').addEventListener('click', createBox, false);
 
 document.getElementById('down').addEventListener('click', moveDown, false);
+
+document.getElementById('up').addEventListener('click', moveUp, false);
+
+document.getElementById('left').addEventListener('click', moveLeft, false);
+
+document.getElementById('right').addEventListener('click', moveRight, false);
+
+document.body.addEventListener('keydown', function (ev) {
+	
+	switch (ev.keyCode ) {
+		case 38: 
+			moveUp();
+			break;
+		case 40:
+			moveDown();
+			break;
+		case 39:
+			moveRight();
+			break;
+		case 37:
+			moveLeft();
+			break;
+		default:
+			// default is for when none of the other cases match
+			console.log(ev.keyCode);
+	}
+}, false);
+
+document.getElementById('color').addEventListener('change', function (ev) {
+	//Inside an event Listener 'this' refers to the elemtn that fired this event
+	console.log(this. value);
+	
+	//`ev.target` refers to the thing that originally dired the event
+	//not necessarily the element the listener is bount to
+	console.log(ev.target.value);
+	
+	//`.value` gets what the user typed into an input field
+	box.style.backgroundColor = this.value;
+}, false);
+
+var removeActiveClass = function() {
+	var divs = document.getElementsByTagName('div');
+	var totalDivs = divs.length;
+	
+	for (var i=0; i<totalDivs; i++) {
+		divs[i].className = 'box';
+	}
+};
+document.documentElement.addEventListener('click', function (ev) {
+	if (ev.target.tagName == 'DIV') {
+		ev.target.className = 'box active';
+		box = ev.target;
+		
+	}
+}, false);
